1985ae50262552b8e5060633096bea9d6878aea7
Juan Jara <jaracalle2041@gmail.com>
2024-02-12 09:41:28 -0500

    Finalizada la tarea 1 (Automata que acepta multiplos de 3 en binario)

---
+++ cpp/tarea_1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string get_chain()
+{
+    string chain;
+    cout << "binary3 ";
+    cin >> chain;
+    return chain;
+}
+
+int delta(int state, string chain, int i)
+{
+    if (chain[i] == ' ') { return state; }
+    switch (state)
+    {
+    case 0:
+        if (chain[i] == '1') { state = 1; }
+        else if (chain[i] == '0') { state = 0; }
+        else { return -1; }
+        break;
+    case 1:
+        if (chain[i] == '1') { state = 0; }
+        else if (chain[i] == '0') { state = 2; }
+        else { return -1; }
+        break;
+    case 2:
+        if (chain[i] == '1') { state = 2; }
+        else if (chain[i] == '0') { state = 1; }
+        else { return -1; }
+        break;     
+    }
+    return delta(state, chain, i+1);
+}
+
+void automata(string chain)
+{
+    int process = delta(0, chain + " ", 0);
+    if (process == 0) 
+    {
+        cout << "The DFA accepts the string '" << chain << "'";
+    } else if (process == -1)
+    {
+        cout << "The DFA don't accept that language";
+    } else 
+    {
+        cout << "The DFA rejects the string '" << chain << "'";
+    }
+}
+
+int main() 
+{   
+    string chain = get_chain();
+    automata(chain);
+    return 0;
+}
